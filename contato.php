0b8abe6e6d295f5dfb71616f08101f94eee4c341
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2018-12-21 14:41:00 -0200

    edit style, responsive and update form

--- contato.php
+++ contato.php
@@ -2,25 +2,25 @@
 
     if(isset($_POST['email']) && !empty($_POST['email'])) {
 
-        $first_nome = addslashes($_POST['name']);
+        $nome = addslashes($_POST['name']);
         $phone = addslashes($_POST['phone']);
         $mail = addslashes($_POST['email']);
         $message = addslashes($_POST['message']);
 
-        $to = "barone@eurobit.com.br";
-        $subject = "Contato - Vitam";
+        $to = "contato@eurobit.com.br";
+        $subject = "Contato - Eurobit";
         $body = "Nome: ".$nome. "\n".
                 "Telefone: ".$phone. "\n".
                 "Email: ".$mail. "\n".
-                "Mensagem".$message;
-        $header = "From:contato@eurobit.com.br"."\r\n".
+                "Mensagem: ".$message;
+        $header = "From:news@eurobit.com.br"."\r\n".
                     "Reply-to:".$mail."\r\n".
                     "X=Mailer:PHP/".phpversion();
 
         if(mail($to,$subject,$body,$header)) {
-            echo("Email enviado com sucesso!");
+            header("Location: ./index.html");
         } else {
-            echo("O Email não pode ser enviado");
+            header("Location: ./index.html");
         }
     }
 
